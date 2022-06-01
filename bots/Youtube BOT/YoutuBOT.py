2d85e246a4c4c69af5cd159e1bdf168b2343017f
Guilherme Henrique Galdini Tosi <galdiniguilherme@usp.br>
2022-06-01 08:10:34 -0300

    Update YoutuBOT.py

--- bots/Youtube BOT/YoutuBOT.py
+++ bots/Youtube BOT/YoutuBOT.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
-from bs4 import BeautifulSoup
 
 class Youtubot():
   def __init__(self):
