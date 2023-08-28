2de6e6a7343aa1726451ed642cd75d0096d5ecd7
Guilherme Henrique Galdini Tosi <galdiniguilherme@usp.br>
2023-08-28 13:47:01 -0300

    Add files via upload

---
+++ DIO/bank_system/bank_system_pooV1/classes/historico.py
@@ -0,0 +1,18 @@
+import datetime
+
+class Historico:
+    def __init__(self):
+        self.transacoes = []
+
+    @property
+    def transacoes(self):
+        return self.transacoes
+
+    def adicionar_transacao(self, transacao):
+        self.transacoes.append(
+            {
+                "tipo": transacao.__class__.__name__,
+                "valor": transacao.valor,
+                "data": datetime.now().strftime("%d-%m-%Y %H:%M:%s"),
+            }
+        )
