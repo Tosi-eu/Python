274d75b1be09dd11be4a82b5722c36f94901344d
Tosi-eu <galdiniguilherme@usp.br>
2024-02-18 18:33:28 -0300

    finished server side of PS challenge

--- Gingalabs/gingalabs/src/main.py
+++ Gingalabs/gingalabs/src/main.py
@@ -16,50 +16,40 @@ if __name__ == "__main__":
     while True:
 
         menu()
-        escolha = input("Opção: ")
-        match escolha:
-            case "1":
-                # Fazer login
-                username = input("Digite seu username: ")
-                password = input("Digite sua senha: ")
-                if marketplace_system.fazer_login(username, password):
-                    print(f"Bem-vindo, {username}!")
-            
-            case "2":
-                # Consultar informações de produtos
-                marketplace_system.consultar_informacoes()
-
-            case "3":
-                # Ver ranking de produtos mais vendidos
-                ranking = marketplace_system.ranking_produtos_mais_vendidos()
-                print("Ranking de produtos mais vendidos:")
-                for produto in ranking:
-                    print(produto)
-
-            case "4":
-                # Cadastrar novo produto
-                nome_produto = input("Digite o nome do novo produto: ")
-                categoria_produto = input("Digite a categoria do novo produto: ")
-                novo_produto = marketplace_system.cadastrar_novo_produto(nome_produto, categoria_produto)
-                if novo_produto:
-                    print(f"Novo produto cadastrado com sucesso: {novo_produto}")
-                else:
-                    print("Falha ao cadastrar o novo produto. Verifique os dados.")
-
-            case "5":
-                # Obter sugestão de reposição de estoque
-                sugestao_reposicao = marketplace_system.obter_sugestao_reposicao_estoque()
-                print(f"Sugestão de reposição de estoque: {sugestao_reposicao}")
-
-            case "6":
-                # Obter sugestão de novos produtos
-                sugestao_novos_produtos = marketplace_system.obter_sugestao_novos_produtos()
-                print(f"Sugestão de novos produtos: {sugestao_novos_produtos}")
-
-            case "7":
-                # Sair do loop
-                print("Saindo do sistema. Até logo!")
-                break
-
-            case _:
-                print("Opção inválida. Tente novamente.")
+        try:
+            escolha = input("Opção: ")
+            match escolha:
+                case "1":
+                    #fazer login
+                    marketplace_system.fazer_login()
+                case "2":
+                    # Consultar informações de produtos
+                    marketplace_system.consultar_informacoes()
+                case "3":
+                    # Ver ranking de produtos mais vendidos
+                    ranking = marketplace_system.ranking_produtos_mais_vendidos()
+                    print("Ranking de produtos mais vendidos:")
+                    for produto in ranking:
+                        print(produto)
+                case "4":
+                    # Cadastrar novo produto
+                    novo_produto = marketplace_system.cadastrar_novo_produto()
+                case "5":
+                    # Obter sugestão de reposição de estoque
+                    sugestao_reposicao = marketplace_system.obter_sugestao_reposicao_estoque()
+                    print(f"Sugestão de reposição de produtos: {sugestao_reposicao}")
+                case "6":
+                    # Obter sugestão de novos produtos
+                    sugestao_novos_produtos = marketplace_system.obter_sugestao_novos_produtos()
+                    print(f"Sugestão de novos produtos: {sugestao_novos_produtos}")
+                case "7":
+                    # Sair do loop
+                    print("Saindo do sistema. Até logo!")
+                    break
+                case _:
+                    print("Opção inválida. Tente novamente.")
+                    
+        except IOError as io:
+            print("Entrada inválida!\n")
+        except Exception as e:
+            print("Erro desconhecido: ", e)
--- Gingalabs/gingalabs/src/sistema.py
+++ Gingalabs/gingalabs/src/sistema.py
@@ -1,12 +1,15 @@
 import json
-import openai
+import random
+import re
+from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import postgrest
 from supabase import create_client
 
 load_dotenv()
-client = openai.OpenAI(
-    api_key=os.environ['OPENAI'],
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY")
 )
 
 class Sistema:
@@ -14,15 +17,19 @@ class Sistema:
         self.supabase_url = os.environ["SUPABASE_PROJECT"]
         self.supabase_key = os.environ["SUPABASE_API_KEY"]
         self.supabase = create_client(self.supabase_url, self.supabase_key)
-        self.vendedor_logado = None
+        self.online= None
 
     #check
-    def fazer_login(self, username, email):
+    def sign_in(self):
+        # Fazer login
+        username = input("Digite seu username: ")
+        email = input("Digite seu email: ")
         response = self.supabase.from_('users').select('*').eq('username', username).eq('email', email).execute()
+
         try:
             if response:
                 user_data = response.dict()
-                self.vendedor_logado = {
+                self.online = {
                         "UserID": user_data['data'][0]['userid'],
                         "Username": user_data['data'][0]['username']
                     }
@@ -32,97 +39,158 @@ class Sistema:
         except IndexError:
             print("Erro ao realizar a verficação do usuário. Credenciais inválidas!")
 
-    def _verificar_login(self):
-        if not self.vendedor_logado:
-            raise Exception("Você precisa fazer login para executar esta operação.")
+    #check
+    def _verify_login(self):
+        if not self.online:
+            print("Usuário não está logado, preecha seus dados para prosseguir!")
+            self.sign_in()
 
     #check
-    def consultar_informacoes(self):
-        self._verificar_login()
-        response = self.supabase.table('products').select('*').eq('owneruserid', self.vendedor_logado["UserID"]).execute()
+    def _validate_product_name(self, nome):
+        pattern = re.compile(r'^[a-zA-Z0-9 _áéíóúâêîôûãõàèìòùäëïöüç]*$')
+
+        if pattern.match(nome):
+            return True
+        else:
+            return False
+  
+    #check
+    def consult_informations(self):
+        self._verify_login()
+        response = self.supabase.table('products').select('*').eq('owneruserid', self.online["UserID"]).execute()
         
         if response:
-            print("Informações dos seus produtos:")
+            print("Informações dos seus products:")
             response = dict(response)
 
-            user_json_path = f'{os.path.abspath(os.getcwd()).replace("\\", "/")}/Gingalabs/gingalabs/Logs/json/{self.vendedor_logado["Username"]}.json'
+            user_json_path = f'{os.path.abspath(os.getcwd())}/Logs/json/{self.online["Username"]}.json'
 
-            user_data = {"produtos": response['data']}
+            user_data = {"products": response['data']}
 
             with open(user_json_path, 'w') as json_file:
                 json.dump(user_data, json_file, indent=2)
 
             print(f'Dados salvos em: {user_json_path}')
             
-            for produto in response['data']:
-                for coluna, valor in produto.items():
+            for product in response['data']:
+                for coluna, valor in product.items():
                     print(f'"{coluna}": "{valor}"')
             print()
         else:
-            print("Nenhum produto encontrado para este vendedor.")
+            print("Nenhum product encontrado para este vendedor.")
+        
+        return response 
+    #check
+    def top_products_best_seller(self):
+        response = self.supabase.table('products').select('productname', 'sales').order('sales', desc=True).execute()
+        response = dict(response)
+        if response:
+            print("Ranking de products mais vendidos:")
+            
+            for product in response['data']:
+                print("Nome do produto:", product['productname'], " Vendas:", product['sales'])
+            print()
+        else:
+            print("Nenhum product encontrado.")
         
         return response
     
     #check
-    def ranking_produtos_mais_vendidos(self):
-        response = self.supabase.table('products').select('productname', 'sales').order('sales', desc=True).execute()
-        response = dict(response)
-        if response:
-            print("Ranking de produtos mais vendidos:")
-            
-            for produto in response['data']:
-                print(f'Nome do produto: {produto['productname']} -> Total de vendas:{produto['sales']}')
-            print()
-        else:
-            print("Nenhum produto encontrado.")
-        
-        return response
-    
-    def cadastrar_novo_produto(self, nome, categoria):
-        self._verificar_login()
-        novo_produto = {
-            "ProductName": nome,
-            "Category": categoria,
-            "OwnerUserID": self.vendedor_logado["UserID"]
-        }
+    def signup_new_product(self):
+        self._verify_login()
+        try:
+            name = input("Digite o nome do novo produto: ")
+            if not self._validate_product_name(name):
+                print("Nome do produto inválido. Permitido apenas underline, acentos e ç.")
+                return
+            description = input("Digite o descrição do novo produto: ")
+            price = float(input("Preço do product: "))
+            category = input("Digite a categoria do novo produto: ")
+            stock_quantity = random.randint(1,100);
+            initial_stock_quantity = stock_quantity + random.randint(1,30)
+            owneruserid = self.online['UserID']
 
-        response = self.supabase.from_('produtos').upsert([novo_produto]).execute()
+            novo_product = {
+                "productname": name,
+                "description": description,
+                "price": price,
+                "stockquantity": stock_quantity,
+                "initialstockquantity": initial_stock_quantity,
+                "category": category,
+                "owneruserid": owneruserid,
+            }
 
-        if response.status_code == 201:
-            return response.json()
-        return None
-    
-    def obter_sugestao_reposicao_estoque(self):
-        self._verificar_login()
+            response = self.supabase.table('products').insert(novo_product).execute()
+            print("product Inserido com sucesso: \n", response.json())
+        except postgrest.exceptions.APIError as e:
+            print(f"Erro ao comunicar-se com o sistema: {e}")
+        except Exception as e2:
+            print(f"Erro desconhecido: {e2}")
 
-        # Consultar produtos com estoque baixo considerando <= 10
-        response = self.supabase.table('products').select('productname', 'sales').eq('owneruserid', self.vendedor_logado["UserID"]).lte('stockquantity', 10).execute()
+    def get_replenish_suggestion(self):
+        self._verify_login()
+
+        # Consultar products com estoque baixo considerando <= 10
+        response = self.supabase.table('products').select('productname', 'sales').eq('owneruserid', self.online["UserID"]).lte('stockquantity', 10).execute()
 
         products = dict(response)
-
-        for produto in products['data']:
-            if products:
-                prompt = f"Os seguintes produtos estão com estoque baixo e precisam ser repostos: {', '.join(produto['productname'])}."
-            else:
-                prompt = "Todos os seus produtos têm estoque suficiente. Não é necessário repor no momento."
-        print()
-
-        # Obter sugestão da OpenAI
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=50
+        low_stock_products = [product['productname'] for product in products['data']]
+        if not low_stock_products:
+            return "Não há produtos para serem reabastecidos no momento"
+        prompt = f"Report which products are low in stock, for replenishing stock: {', '.join(low_stock_products)}"
+        
+        completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a assistant, skilled for recommending which products that have to replenish stock."},
+            {"role": "user", "content": prompt}],
         )
 
-        return response.choices[0].text.strip()
+        log_filename = f"{os.path.abspath(os.getcwd())}/Logs/openai/Reestock_logs/{self.online['Username']}_reestock_log.json"
+        with open(log_filename, 'w') as log_file:
+            log_data = {
+                "username": self.online['Username'],
+                "operation": "reestock_suggestion",
+                "products": [
+                    {"product_name": product['productname'], "message": completion.choices[0].message.content.strip()}
+                    for product in products['data']
+                ]
+            }
+            json.dump(log_data, log_file, indent=2)
 
+        return completion.choices[0].message.content.strip()
 
-    def obter_sugestao_novos_produtos(self):
-        self._verificar_login()
-        prompt = "Quais novos produtos você poderia considerar vender?"
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=50
+    def get_suggestion_to_buy_new_products(self):
+        self._verify_login()
+
+        # Consultar products top5 mais vendidos
+        response_ranking = self.supabase.table('products').select('productname', 'sales').order('sales', desc=True).limit(5).execute()
+        products_ranking = dict(response_ranking)
+
+        if not products_ranking or not products_ranking['data']:
+            return "Não há products disponíveis para sugestão no momento!"
+
+        top_selling_products = [product['productname'] for product in products_ranking['data']]
+
+        prompt = f"Consider to sell newer products based in our best sellers: {', '.join(top_selling_products)}"
+        
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are an assistant, skilled for recommending new products."},
+                {"role": "user", "content": prompt}],
         )
-        return response.choices[0].text.strip()
+        
+        log_filename = f"{os.path.abspath(os.getcwd())}/Logs/openai/Top_sold_logs/{self.online['Username']}_top_sold.json"
+        with open(log_filename, 'w') as log_file:
+            log_data = {
+                "username": self.online['Username'],
+                "operation": "buy_suggestions",
+                "products": [
+                    {"product_name": product['productname'], "message": completion.choices[0].message.content.strip()}
+                    for product in products_ranking['data']
+                ]
+            }
+            json.dump(log_data, log_file, indent=2)
+
+        return completion.choices[0].message.content.strip()
