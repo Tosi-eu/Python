e7307401fba675779a8123ae1e8d6740547e24ff
Tosi-eu <galdiniguilherme@usp.br>
2024-12-01 02:14:11 -0300

    refazendo o programa

--- Operational Research/Integer Optimization/script.py
+++ Operational Research/Integer Optimization/script.py
@@ -1,10 +1,7 @@
 from numpy import array, argwhere
 from pulp import LpProblem, LpVariable, LpMaximize, lpSum
-from networkx import get_node_attributes, draw, Graph
-from matplotlib.pyplot import figure, title, gca, show
-
-MATRIX_FILENAME = 'problem.txt'
-POSITIONS_FILENAME = 'positions.txt'
+import networkx as nx
+import matplotlib.pyplot as plt
 
 VERTEX_COSTS = {
     "Casa": 25600,
@@ -12,7 +9,7 @@ VERTEX_COSTS = {
     "Fábrica": 19200
 }
 
-def createVars(N: int):
+def create_binary_vars(N: int):
     try:
         x_C = array([LpVariable(f"x_C_{i}", cat="Binary") for i in range(N)])
         x_P = array([LpVariable(f"x_P_{i}", cat="Binary") for i in range(N)])
@@ -25,17 +22,20 @@ def createVars(N: int):
 
     return  x_C, x_P, x_F, z_CP, z_CF, z_PF
 
-def read_matrix_from_file(nome_arquivo):
-    matriz = []
+def read_matrix_from_file(filename: str):
+    matrix = []
     try:
-        with open(nome_arquivo, 'r') as file:
-                return array([list(map(float, linha.split())) for linha in file])
-    except ValueError as e:
+        with open(filename, 'r') as file:
+            return array([list(map(float, linha.split())) for linha in file])
+    except Exception as e:
         print(f"Erro ler arquivo: {e}")
 
-    return matriz
+    return matrix
 
-def get_node_colors(solution):
+def create_graph(A, positions, solution, happiness):
+    graph = nx.Graph()
+    profit = 0
+
     node_colors = []
     for i in range(len(A)):
         if solution[i] == 'Casa':
@@ -44,70 +44,97 @@ def get_node_colors(solution):
             node_colors.append('blue')  # Fábrica: azul
         else:
             node_colors.append('green')  # Parque: verde
-
-    return node_colors
-
-def set_edge_colors(matrix, graph, solution, happiness, profit):
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            if A[i][j] == 1:
-                if (solution[i] == "Casa" and solution[j] == "Parque") or (solution[i] == "Parque" and solution[j] == "Casa"):
-                    edge_color = 'green'  # Casa - Parque: verde
-                    happiness += 1
-                elif (solution[i] == "Casa" and solution[j] == "Fábrica") or (solution[i] == "Fábrica" and solution[j] == "Casa"):
-                    edge_color = 'blue'  # Casa - Fábrica: azul
-                    profit += 1
-                elif (solution[i] == "Parque" and solution[j] == "Fábrica") or (solution[i] == "Fábrica" and solution[j] == "Parque"):
-                    edge_color = 'red'  # Parque - Fábrica: vermelha
-                    happiness += -1
-                else:
-                    edge_color = "gray"
-                
-                graph.add_edge(i, j, color=edge_color)
-
-    return graph, happiness, profit
-
-def create_graph(A, positions, solution, happiness):
-    G = Graph()
-    profit = 0
-
-    node_colors = get_node_colors(solution)
-
-    for i in range(len(A)):
-        G.add_node(i, positions=positions[i])
-
-    G, happiness, profit = set_edge_colors(A, G, solution, happiness, profit)
-
-    positions = get_node_attributes(G, 'positions')
-    edge_colors = [G[u][v]['color'] for u, v in G.edges()]
-
-    return profit, happiness, G, positions, node_colors, edge_colors
-
-def plot_graph(graph, positions, node_colors, edge_colors, total_cost, happiness, profit, inhabitants):
-    figure(figsize=(10, 10))
-    draw(graph, positions, with_labels=True, node_size=250, node_color=node_colors, font_size=8, font_weight='bold', edge_color=edge_colors)
-    title("Grafo do Problema de Otimização")
-
-    legend_text = (f"Custo Total: {total_cost}\n"
-                   f"Habitantes: {inhabitants}\n"
-                   f"Felicidade: {happiness}\n"
-                   f"Lucro: {profit}"
-                   )
     
-    gca().legend(
-        [legend_text],
-        loc="upper right",
-        handlelength=0, 
-        handletextpad=0, 
-        fontsize=10,
-        frameon=True,  
-    )
-    show()
+    for i in range(len(A)):
+        graph.add_node(i, pos=positions[i])
 
-def get_solutions(N, x_C, x_P, x_F):
-    total_cost = 0
-    inhabitants = 0
+    idx = argwhere(A == 1)
+    for i, j in idx:
+        if (solution[i] == "Casa" and solution[j] == "Parque") or (solution[i] == "Parque" and solution[j] == "Casa"):
+            edge_color = 'green'  # Casa - Parque: verde
+            happiness += 1
+        elif (solution[i] == "Casa" and solution[j] == "Fábrica") or (solution[i] == "Fábrica" and solution[j] == "Casa"):
+            edge_color = 'blue'  # Casa - Fábrica: azul
+            profit += 1
+        elif (solution[i] == "Parque" and solution[j] == "Fábrica") or (solution[i] == "Fábrica" and solution[j] == "Parque"):
+            edge_color = 'red'  # Parque - Fábrica: vermelha
+            happiness += -1
+        else:
+            edge_color = 'gray'
+        graph.add_edge(i, j, color=edge_color)
+
+    pos = nx.get_node_attributes(graph, 'pos')
+    edge_colors = [graph[u][v]['color'] for u, v in graph.edges()]
+
+    return graph, profit, happiness, pos, edge_colors, node_colors
+
+def plot_graph(graph, position, node_colors, edge_colors, output_filename="graph.png"):
+    plt.figure(figsize=(10, 10))
+    nx.draw(graph, position, with_labels=True, node_size=175, node_color=node_colors, font_size=8, font_weight='bold', edge_color=edge_colors)
+    plt.title("Grafo do Problema de Otimização")
+    plt.savefig(output_filename)
+    plt.show()
+
+if __name__ == "__main__":
+    MATRIX_FILENAME = 'problem.txt'
+    POSITIONS_FILENAME = 'positions.txt'
+    OUTPUT_FILENAME = 'solution.txt' 
+    
+    A = array(read_matrix_from_file(MATRIX_FILENAME))
+    positions = array(read_matrix_from_file(POSITIONS_FILENAME))
+    N = len(A) 
     happiness = 0
+    inhabitants = 0
+    total_cost = 0
+
+    # Problema
+    model = LpProblem("Plano Diretor da Cidade", LpMaximize)
+
+    x_C, x_P, x_F, z_CP, z_CF, z_PF = create_binary_vars(N)
+
+    # Função objetivo
+    L = lpSum(z_CF[i][j] * A[i][j] for i in range(N) for j in range(N))
+    F = lpSum(
+        x_P[i] - x_F[i] + lpSum(z_CP[i][j] - z_PF[i][j] for j in range(N) if A[i][j])
+        for i in range(N)
+    )
+
+    model += (2 * N ** 2 + 1) * L + F
+
+    # Restrições
+    for i in range(N):
+        model += x_C[i] + x_P[i] + x_F[i] == 1
+        if i == 0:
+            model += x_C[i] == 1  # Vértice 1 deve ser uma Casa
+
+        for j in range(N):
+            if A[i][j]:
+                model += x_C[i] + x_F[j] - 1 <= z_CF[i][j]
+                model += x_C[i] >= z_CF[i][j]
+                model += x_F[j] >= z_CF[i][j]
+
+                model += x_C[i] + x_P[j] - 1 <= z_CP[i][j]
+                model += x_C[i] >= z_CP[i][j]
+                model += x_P[j] >= z_CP[i][j]
+
+                model += x_P[i] + x_F[j] - 1 <= z_PF[i][j]
+                model += x_P[i] >= z_PF[i][j]
+                model += x_F[j] >= z_PF[i][j]
+
+    for i in range(N): 
+        for j in range(N): 
+            if A[i][j] == 1.0: 
+                model += z_CF[i][j] <= x_C[i]
+                model += z_CF[i][j] <= x_F[j]
+                model += z_CP[i][j] <= x_C[i]
+                model += z_CP[i][j] <= x_P[j]
+                model += z_PF[i][j] <= x_P[i]
+                model += z_PF[i][j] <= x_F[j]
+   
+    model += F >= 0
+    model += happiness >= 0
+
+    model.solve()
 
     solution = []
     for i in range(N):
@@ -124,70 +151,19 @@ def get_solutions(N, x_C, x_P, x_F):
             total_cost += VERTEX_COSTS["Fábrica"]
             happiness += -1
 
-    return solution, total_cost, inhabitants, happiness
+    graph, profit, happiness, pos, edge_colors, node_colors = create_graph(A, positions, solution, happiness)
 
-if __name__ == "__main__":
-    A = array(read_matrix_from_file(MATRIX_FILENAME))
-    positions = array(read_matrix_from_file(POSITIONS_FILENAME))
-    N = len(A)
-    happiness = 0
+    plot_graph(graph, pos, node_colors, edge_colors)
 
-    model = LpProblem("Plano Diretor da Cidade", LpMaximize)
-
-    # Variáveis
-    x_C, x_P, x_F, z_CP, z_CF, z_PF = createVars(N)
-
-    # Função objetivo
-    L = lpSum((z_CF * A).flatten())
-    F = lpSum(
-        x_P[i] - x_F[i] + lpSum(z_CP[i][j] - z_PF[i][j] for j in range(N) if A[i][j])
-        for i in range(N)
-    )
-
-    model += (2 * N ** 2 + 1) * L + F
-
-    # Restrição das variáveis binárias
-    for i in range(N):
-        model += x_C[i] + x_P[i] + x_F[i] == 1
-        if i == 0:
-            model += x_C[i] == 1  # Vértice 1 deve ser uma Casa
-
-        for j in range(N):
-            if A[i][j]:
-                # Verificações para z_CF (Casa-Fábrica), z_CP (Casa-Parque), e z_PF (Parque-Fábrica)
-                model += z_CF[i][j] <= x_C[i], f"z_CF_cond1_{i}_{j}"
-                model += z_CF[i][j] <= x_F[j], f"z_CF_cond2_{i}_{j}"
-                model += z_CF[i][j] >= x_C[i] + x_F[j] - 1, f"z_CF_cond3_{i}_{j}"
-
-                model += z_CP[i][j] <= x_C[i], f"z_CP_cond1_{i}_{j}"
-                model += z_CP[i][j] <= x_P[j], f"z_CP_cond2_{i}_{j}"
-                model += z_CP[i][j] >= x_C[i] + x_P[j] - 1, f"z_CP_cond3_{i}_{j}"
-
-                model += z_PF[i][j] <= x_P[i], f"z_PF_cond1_{i}_{j}"
-                model += z_PF[i][j] <= x_F[j], f"z_PF_cond2_{i}_{j}"
-                model += z_PF[i][j] >= x_P[i] + x_F[j] - 1, f"z_PF_cond3_{i}_{j}"
-
-    # Restrições de poda: verificações adicionais
-    indices = argwhere(A == 1)
-    for i, j in indices:
-            model += z_CF[i][j] <= x_C[i]
-            model += z_CF[i][j] <= x_F[j]
-            model += z_CP[i][j] <= x_C[i]
-            model += z_CP[i][j] <= x_P[j]
-            model += z_PF[i][j] <= x_P[i]
-            model += z_PF[i][j] <= x_F[j]
-    
-    # Poda de happineess e profit (não precisa ser negativa)
-    model += F >= 0
-    model += happiness >= 0
-
-    model.solve()
-
-    # Verificar solução
-    solution, total_cost, inhabitants, happiness = get_solutions(N, x_C, x_P, x_F)
-
-    # Plotar o grafo com as cores
-    profit, happiness, G, positions, node_colors, edge_colors = create_graph(A, positions, solution, happiness)
-
-    # grafo e métricas 
-    plot_graph(G, positions, node_colors, edge_colors, total_cost, happiness, profit, inhabitants)
+    try:
+        with open(OUTPUT_FILENAME, 'w') as file:
+            file.write("Solucao Otima:\n")
+            for i, s in enumerate(solution):
+                file.write(f"Vertice {i + 1}: {s}\n")
+            file.write(f"\nCusto Total: {total_cost}\n")
+            file.write(f"Numero de Habitantes: {inhabitants}\n")
+            file.write(f"Nivel de Felicidade: {happiness}\n")
+            file.write(f"Lucro Total: {profit}\n")
+        print(f"Solução salva em '{OUTPUT_FILENAME}'.")
+    except Exception as e:
+        print(f"Erro ao salvar a solução: {e}")
